29a894c9c3b9233d8d77f286b550c2101f510d36
Brian Shi <brishi@seas.upenn.edu>
2014-09-12 23:19:39 -0400

    added models and collections

---
+++ public/scripts/models/subscription.js
@@ -0,0 +1,25 @@
+define([
+	"jquery",
+	"underscore",
+	"backbone",
+	"events",			// event aggregator
+
+	], function ( $, _, Backbone, vent ) { 
+		var Subscription = Backbone.Model.extend({ 
+			defaults: { 
+			},
+
+			initialize: function () { 
+			},
+
+			idAttribute: ,
+
+			validate: function () { 
+			}
+		});
+
+		return Subscription;
+	}
+);
+
+
