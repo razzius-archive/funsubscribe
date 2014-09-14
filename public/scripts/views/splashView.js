7d97a61c5f3bd3ee97d03443c8e075d513b16798
Razzi Abuissa <razzi53@gmail.com>
2014-09-13 21:46:26 -0400

    Animate envelopes in

--- public/scripts/views/splashView.js
+++ public/scripts/views/splashView.js
@@ -7,27 +7,26 @@ define([
 
 	], function (	$, _, Backbone, vent,
 					splashTemplate
-				) { 
+				) {
 
-		var SplashView = Backbone.View.extend({ 
+		var SplashView = Backbone.View.extend({
 			template: _.template( splashTemplate ),
 
 			className: "splash-body",
 
-			events: { 
+			events: {
 				"click .login": "submitLogin",
 			},
 
 			initialize: function () {
 			},
 
-			render: function () { 
+			render: function () {
 				$(this.el).html( this.template() );
-
 				return this;
 			},
 
-			submitLogin: function () { 
+			submitLogin: function () {
 				$(".login-email").val() || $(".login-password").val() ? vent.trigger( "loginSubmit" ) : alert("Please enter your email and password") ;		// Make this alert sexier
 			}
 
